d6e5eff5713547572351bca8459b590e4ec01408
balax-studio <balax-studio@users.noreply.github.com>
2026-09-22 10:47:19 +0000

    deploy: 636c7493d7e8e415b22642a174172c339269c03d

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"af7e796e161ae0bb1ff0758c71a7105418bd9d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "505498649" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1830760732" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
